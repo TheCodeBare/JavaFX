719b2ac8c297884f9589a8756d3aa316d4ce1d70
TheCodeBare <aymericF.apb@outlook.fr>
2024-03-27 11:43:25 +0100

    aa

--- ProjetIHM/DatabaseConnector.java
+++ ProjetIHM/DatabaseConnector.java
@@ -1,3 +1,5 @@
+package ProjetIHM;
+
 import java.sql.*;
 import java.util.Scanner;
 
--- ProjetIHM/Stage.java
+++ ProjetIHM/Stage.java
@@ -1,8 +1,10 @@
+package ProjetIHM;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+  
 public class Stage {
 
     // Méthode pour ajouter un stage
