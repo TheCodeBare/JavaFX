e13ea29f9d38b5aad3f9445b91e6d85724315522
TheCodeBare <aymericF.apb@outlook.fr>
2024-03-29 11:45:39 +0100

    Modification des controllers - Chargement des pages dans le même ContentPane et plus dans des fenêtres séparées

--- Controller.java
+++ Controller.java
@@ -1,61 +1,45 @@
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuBar;
+import javafx.scene.layout.Pane;
 import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 
 public class Controller {
-    
+
     @FXML
-    private MenuBar menubarre;
-    
-    @FXML
-    private Button saisirUnStageButton;
-    @FXML
-    private Button btnListeStages;
-    
+    private Pane contentPane;
+
+    private Stage primaryStage;
+
+    public void setPrimaryStage(Stage stage) {
+        this.primaryStage = stage;
+    }
+
     @FXML
     public void initialize() {
         // Vous pouvez initialiser des éléments ici si nécessaire
     }
 
-
-
-
-    
     @FXML
     public void handleSaisirUnStageButtonClick(ActionEvent event) {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("Page_Ajout_Stage.fxml"));
-        Parent root;
+        loadPage("Page_Ajout_Stage.fxml", "Page Ajout Stage");
+    }
+
+    @FXML
+    public void handleListeStageButtonClick(ActionEvent event) {
+        loadPage("Page 4 - Tableau de stage.fxml", "Page liste Stage");
+    }
+
+    private void loadPage(String fxmlFile, String title) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
         try {
-            root = loader.load();
-            Scene scene = new Scene(root);
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.setTitle("Page Ajout Stage");
-            stage.show();
+            Pane root = loader.load();
+            contentPane.getChildren().setAll(root);
+            primaryStage.setTitle(title);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-        @FXML
-    public void handleListeStageButtonClick(ActionEvent event) {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("Page 4 - Tableau de stage.fxml"));
-            Parent root;
-            try {
-            root = loader.load();
-            Scene scene = new Scene(root);
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.setTitle("Page liste Stage");
-            stage.show();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
 }
-
+    
