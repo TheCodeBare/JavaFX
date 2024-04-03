dd44c27e69b13a511dffa4fd9e883bd4d49da9ba
TheCodeBare <aymericF.apb@outlook.fr>
2024-04-03 11:58:43 +0200

    Version  FLo - Connexion réussie avec la BBD + Ajout de stage fonctionnel

--- Controller.java
+++ Controller.java
@@ -1,33 +1,35 @@
 import java.io.IOException;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.Pane;
-import javafx.event.ActionEvent;
-import javafx.stage.Stage;
-import javafx.application.Platform;
-import javafx.scene.control.TextField;
 import java.sql.Connection;
 import java.sql.SQLException;
-
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class Controller {
     private Connection conn;
+    private Stage primaryStage;
+
     @FXML
     private Pane contentPane;
 
-    private Stage primaryStage;
-    
     @FXML
     private TextField promotionField;
+
     @FXML
     private TextField siegeField;
+
     @FXML
     private TextField sujetStageField;
+
     @FXML
     private TextField debutMoisField;
+
     @FXML
     private TextField dureeField;
-    
 
     public void setPrimaryStage(Stage stage) {
         this.primaryStage = stage;
@@ -50,6 +52,38 @@ public class Controller {
         loadPage("Page 4 - Tableau de stage.fxml", "Page liste Stage");
     }
 
+    @FXML
+    public void handleAideButtonClick(ActionEvent event) {
+        loadPage("Page 5 - Aide.fxml", "Page Aide");
+    }
+
+    public void handleQuitterButtonClick(ActionEvent event) {
+        // Fermer proprement l'application
+        Platform.exit();
+    }
+
+    @FXML
+    public void ajouterStageFromForm() {
+        String promotion = promotionField.getText();
+        String siege = siegeField.getText();
+        String sujetStage = sujetStageField.getText();
+        String debutMois = debutMoisField.getText();
+        String duree = dureeField.getText();
+        conn = DatabaseConnector.connect();
+
+        try {
+            // Appel à la méthode pour ajouter le stage dans la base de données
+            DatabaseConnector.ajouterStage(conn, siege, sujetStage, debutMois, Integer.parseInt(duree), promotion);
+
+            // Vous pouvez ajouter ici un message de confirmation à l'utilisateur
+
+            // Effacer les champs après l'ajout
+            clearFields();
+        } catch (SQLException e) {
+            e.printStackTrace(); // Gérez l'exception de manière appropriée
+        }
+    }
+
     private void loadPage(String fxmlFile, String title) {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
         try {
@@ -60,40 +94,12 @@ public class Controller {
             e.printStackTrace();
         }
     }
-    
-    @FXML
-    public void handleAideButtonClick(ActionEvent event) {
-        loadPage("Page 5 - Aide.fxml", "Page Aide");
+
+    private void clearFields() {
+        promotionField.clear();
+        siegeField.clear();
+        sujetStageField.clear();
+        debutMoisField.clear();
+        dureeField.clear();
     }
-    
-    public void handleQuitterButtonClick(ActionEvent event) {
-        // Fermer proprement l'application
-        Platform.exit();
-    }
-    @FXML
-    public void ajouterStageFromForm() {
-        String promotion = promotionField.getText();
-        String siege = siegeField.getText();
-        String sujetStage = sujetStageField.getText();
-        String debutMois = debutMoisField.getText();
-        String duree = dureeField.getText();
-
-        try {
-            // Appel à la méthode pour ajouter le stage dans la base de données
-            DatabaseConnector.ajouterStage(conn, siege, sujetStage, debutMois, Integer.parseInt(duree), promotion);
-
-            // Vous pouvez ajouter ici un message de confirmation à l'utilisateur
-
-            // Effacer les champs après l'ajout
-            promotionField.clear();
-            siegeField.clear();
-            sujetStageField.clear();
-            debutMoisField.clear();
-            dureeField.clear();
-        } catch (SQLException e) {
-            e.printStackTrace(); // Gérez l'exception de manière appropriée
-        }
-    }
-
 }
-    
